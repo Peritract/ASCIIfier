12e93bb02fd7b21c9aa40691917d20a302d28271
Peritract <peritract@hotmail.co.uk>
2018-06-10 10:03:00 +0100

    General improvements
    Cleaned up the code, moved things into functions, simplified various parts, and made the whole thing a little more robust.

--- ASCIIfier.py
+++ ASCIIfier.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-ASCII_CHARS = [" .:-=+*#%@", "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'.", "@%#*+=-;:. "]
-#1 and 3 are flipped versions of each other.
+ASCII_CHARS = [" .:-=+*#%@", "@%#*+=-;:. ", "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'."]
+#1 and 2 are flipped versions of each other.
 CHARSET = 0
 WIDTH = 120
 
@@ -28,45 +28,65 @@ def ASCIIfy_image(path, chars, width=150):
     chars = map_chars(img, chars)
     n_img = [chars[index: index + width] for index in range(0, len(chars), width)]
     return "\n".join(n_img)
-    
 
-print("###############################")
-print("#        THE ASCIIFIER        #")
-print("###############################")
-print("Welcome to the ASCIIfier.")
-print("Use this program to convert images to ASCII.")
-print("Type '$help' for more information.")
-print("Type a filename to begin.")
-print()
-while True:
-    command = input(">")    
-    if command[0] == "$":
-        command = command.split(" ")
-        if command[0] == "$width":
-            if int(command[1]):
-               WIDTH = int(command[1])
-               print("Width has been set to", command[1] + ".")
-            else:
-                print("Invalid command")
-        elif command[0] == "$charset":
-            if len(ASCII_CHARS) - 1 >= int(command[1]):
-                CHARSET = int(command[1])
-                print("Charset has been set to", command[1] + ".")
-            else:
-                print("No such charset found.")
-        elif command[0] == "$help":
-            print("ASCIIfier HELP:")
-            print("Enter the name of an image file (plus extension) and then press 'Enter.'")
-            print("The program will output your ASCIIfied image.")
-            print("Enter '$width ' and then a number to change the default width.")
-            print("Enter '$charset ' and then a number to change the default charset.")     
-        else:
-            print("Invalid command.")
+def set_width(width):
+    global WIDTH
+    WIDTH = int(width)
+    print("Width has been set to", str(width) + ".")
+
+def set_charset(num):
+    global CHARSET
+    if len(ASCII_CHARS) - 1 >= num:
+        CHARSET = num
+        print("Charset has been set to", str(num) + ".")
     else:
-        try:
-            print(ASCIIfy_image(command, ASCII_CHARS[CHARSET], WIDTH))
-            print()
-            print("Enter another filename to convert another image.")
-        except:
-            print("Could not convert", command +".", "Sorry.")
+        print("No such charset found.")
+
+def can_int(x):
+    #helper function - checks if something can be an integer
+    try:
+        int(x)
+        return True
+    except:
+        return False
+
+def help():
+    print("ASCIIfier HELP:")
+    print("Enter the name of an image file (plus extension) and then press 'Enter.'")
+    print("The program will output your ASCIIfied image.")
+    print("Enter '$width ' and then a number to change the default width.")
+    print("Enter '$charset ' and then a number to change the default charset.") 
+
+def main():
+    print("###############################")
+    print("#        THE ASCIIFIER        #")
+    print("###############################")
+    print("Welcome to the ASCIIfier.")
+    print("Use this program to convert images to ASCII.")
+    print("Type '$help' for more information.")
+    print("Type a filename to begin.")
     print()
+    
+    while True:
+        command = input(">")    
+        if command[0] == "$":
+            command = command.split(" ")
+            print(command[1])
+            if len(command) > 1 and can_int(command[1]):
+                if command[0] == "$width":
+                    set_width(int(command[1]))
+                elif command[0] == "$charset":
+                    set_charset(int(command[1]))
+            elif command[0] == "$help":
+                help()    
+            else:
+                print("Invalid command.")
+        else:
+            try:
+                print(ASCIIfy_image(command, ASCII_CHARS[CHARSET], WIDTH))
+            except:
+                print("ERROR: Unable to convert", command)
+        print()
+
+if __name__ == "__main__":
+    main()
